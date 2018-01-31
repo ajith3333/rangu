4e79f8d2cc4ba186518e98fbbc4d2556eced9cea
ajith3333 <35687948+ajith3333@users.noreply.github.com>
2018-01-31 15:54:52 +0530

    sum2num.c

---
+++ sum2num.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+int a,b,c;
+printf("enter the a and b values");
+scanf("%d%d",&a,&b);
+c=a+b;
+printf("the sum of the 2 number is");
+scanf("%d",&c);
+return 0;
+}
